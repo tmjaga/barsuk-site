2963418e6b3d09d962b855c7a53109c94773d6d9
Anatoly Meshkov <tmjaga@gmail.com>
2026-02-12 11:33:27 +0200

    feat: Delete expired orders by order_start date artisan command added.

---
+++ app/Console/Commands/DeleteExpiredOrders.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Enums\OrderStatus;
+use App\Models\Order;
+use Carbon\Carbon;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Validator;
+
+class DeleteExpiredOrders extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'orders:delete-expired {days_count?}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Delete expired orders by order_start date';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle(): int
+    {
+        $days = $this->argument('days_count') ?? 3;
+        $date = Carbon::now()->subDays($days);
+
+        $validator = Validator::make(
+            ['days_count' => $days],
+            ['days_count' => ['required', 'integer', 'min:1']]
+        );
+
+        if ($validator->fails()) {
+            $this->error($validator->errors()->first('days_count'));
+
+            return self::FAILURE;
+        }
+
+        $expiredOrders = Order::where('order_start', '<', $date)->pluck('id');
+
+        DB::table('order_service')
+            ->whereIn('order_id', $expiredOrders)
+            ->delete();
+
+        $deleted = Order::whereIn('id', $expiredOrders)->delete();
+
+        $this->info("Deleted {$deleted} expired orders older than {$days} day(s).");
+
+        return self::SUCCESS;
+    }
+}
