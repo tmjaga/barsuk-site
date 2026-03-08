5d74fe49746dad1b5f0af198eb664b3376f9e5d4
Anatoly Meshkov <tmjaga@gmail.com>
2026-03-08 13:57:57 +0200

    feat: Clear formatting button added in to the Trix editor

--- resources/js/trix-extensions.js
+++ resources/js/trix-extensions.js
@@ -53,4 +53,26 @@ document.addEventListener("trix-initialize", function (event) {
         editor.activateAttribute("fontSize", e.target.value)
     })
 
-})
+    const clearButton = document.createElement("button")
+    clearButton.type = "button";
+    clearButton.className = "trix-button";
+    clearButton.title = "Clear formatting";
+    clearButton.id = "clear-formatting";
+    clearButton.innerHTML = `
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+      <path stroke-linecap="round" stroke-linejoin="round" d="m20.25 7.5-.625 10.632a2.25 2.25 0 0 1-2.247 2.118H6.622a2.25 2.25 0 0 1-2.247-2.118L3.75 7.5m6 4.125 2.25 2.25m0 0 2.25 2.25M12 13.875l2.25-2.25M12 13.875l-2.25 2.25M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125Z" />
+    </svg>
+    `;
+
+    group.appendChild(clearButton);
+
+    clearButton.addEventListener("click", () => {
+        editor.deactivateAttribute("bold")
+        editor.deactivateAttribute("italic")
+        editor.deactivateAttribute("strike")
+
+        editor.deactivateAttribute("color")
+        editor.deactivateAttribute("background")
+        editor.deactivateAttribute("fontSize")
+    });
+});
